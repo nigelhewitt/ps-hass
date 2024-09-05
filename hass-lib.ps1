3f424cbe9ffe84aa27484bc346c349632054850d
Nigel V Hewitt <nigelh@combro.co.uk>
2024-09-05 18:49:49 +0100

    Initial

---
+++ hass-lib.ps1
@@ -0,0 +1,57 @@
+﻿################################################################################
+#
+#  hass-lib.ps1			functions to control Home Assistant
+#
+################################################################################
+
+Function Get-HassURL
+{
+	"http://192.168.1.201:8123"
+}
+Function Get-HassToken
+{
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkZWViMWI0MDk1ZDY0MDZjYTg0Yzg5ZjU4YjRmNmI2MSIsImlhdCI6MTcyMjE5NTIyNSwiZXhwIjoyMDM3NTU1MjI1fQ.aPcw8vgzhJuWdJRkYlXwcpdh4a369TZdxs3mZNZx5G8'
+}
+
+Function Set-Hass {
+	Param(
+        [Parameter(Mandatory=$true)][string]$object,
+        [Parameter(Mandatory=$true)][string]$action,
+        [Parameter(Mandatory=$true)][string]$domain
+    )
+    $hash    = @{ entity_id = "$object" }
+    $json    = $hash | convertto-json
+    $headers = @{ 'Authorization' = Get-HassToken }
+    $url     = (Get-HassURL) + "/api/services/" + $domain + "/" + $action
+    $res     = Invoke-WebRequest -uri $url -Headers $headers -Method POST -Body $json
+}
+
+Function Get-Hass {
+	Param(
+        [Parameter(Mandatory=$true)][string]$object
+    )
+    $headers = @{ 'Authorization' = Get-HassToken }
+    $url = (Get-HassURL) + "/api/states/" + $object
+    $res = Invoke-WebRequest -uri $url -Headers $headers -Method GET
+    $res.Content | ConvertFrom-Json
+}
+
+Function Set-ShortcutIcon {
+	Param (
+		[Parameter(Mandatory=$true)][string]$target,
+		[Parameter(Mandatory=$true)][string]$icon		# append ,2 for index
+	)
+	$DesktopPath = [Environment]::GetFolderPath("Desktop") + "\$target"
+	$WshShell = New-Object -comObject WScript.Shell
+	$Shortcut = $WshShell.CreateShortcut($DesktopPath)
+	$Shortcut.IconLocation = $icon
+	$Shortcut.Save()
+}
+
+
+Set-Hass -domain "switch" -action "toggle" -object "switch.tasmota3"
+Start-Sleep -Seconds 2
+$x = Get-Hass -object "switch.tasmota3"
+if($x.state -match 'on'){ $icon = "D:\Util\power-green-64.ico" }
+else					{ $icon = "D:\Util\power-red-64.ico"   }
+Set-ShortcutIcon -target "charger.lnk"  -icon $icon
---
+++ hass.ps1
@@ -0,0 +1,93 @@
+﻿################################################################################
+#
+# automate a Home Assistant call
+#
+# use as
+#   ./hass.ps1 -domain switch -action toggle -object switch.sonoff05
+#
+# or in a shortcut as
+#   "C:\Program Files\PowerShell\7\pwsh.exe" D:\Util\hass.ps1 -domain switch `
+#			 -action toggle -object switch.tas03 -link charger.lnk
+#			-officon D:\Util\power-red-64.ico -onicon D:\Util\power-green-64.ico
+#
+# to update the icon
+#   onit the -action value
+
+# domain
+#   switch etc.
+#
+# action
+#   toggle | turn_on | turn_off  as appropriate
+#
+# object
+#   entity id as switch.sonoff05
+#
+################################################################################
+
+Param(
+  [Parameter(Mandatory=$true)][string]$object,
+  [Parameter()][string]$action=$null,
+  [Parameter(Mandatory=$true)][string]$domain,
+  [Parameter()][string]$link=$null,
+  [Parameter(Mandatory=$true)][string]$officon,
+  [Parameter(Mandatory=$true)][string]$onicon
+)
+
+Function Get-HassURL
+{
+	"http://192.168.1.201:8123"
+}
+
+Function Get-HassToken
+{
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkZWViMWI0MDk1ZDY0MDZjYTg0Yzg5ZjU4YjRmNmI2MSIsImlhdCI6MTcyMjE5NTIyNSwiZXhwIjoyMDM3NTU1MjI1fQ.aPcw8vgzhJuWdJRkYlXwcpdh4a369TZdxs3mZNZx5G8'
+}
+
+Function Set-Hass {
+	Param(
+        [Parameter(Mandatory=$true)][string]$object,
+        [Parameter(Mandatory=$true)][string]$action,
+        [Parameter(Mandatory=$true)][string]$domain
+    )
+    $hash    = @{ entity_id = "$object" }
+    $json    = $hash | convertto-json
+    $headers = @{ 'Authorization' = Get-HassToken }
+    $url     = (Get-HassURL) + "/api/services/" + $domain + "/" + $action
+    $res     = Invoke-WebRequest -uri $url -Headers $headers -Method POST -Body $json
+}
+
+Function Get-Hass {
+	Param(
+        [Parameter(Mandatory=$true)][string]$object
+    )
+    $headers = @{ 'Authorization' = Get-HassToken }
+    $url = (Get-HassURL) + "/api/states/" + $object
+    $res = Invoke-WebRequest -uri $url -Headers $headers -Method GET
+    $res.Content | ConvertFrom-Json
+}
+
+Function Set-ShortcutIcon {
+	Param (
+		[Parameter(Mandatory=$true)][string]$target,
+		[Parameter(Mandatory=$true)][string]$icon		# append ,2 for index
+	)
+	$DesktopPath = [Environment]::GetFolderPath("Desktop") + "\$target"
+	$WshShell = New-Object -comObject WScript.Shell
+	$Shortcut = $WshShell.CreateShortcut($DesktopPath)
+	$Shortcut.IconLocation = $icon
+	$Shortcut.Save()
+}
+
+# if there is an action perform the action
+if($action -ne $null){
+   Set-Hass -domain $domain -action $action -object $object
+   Start-Sleep -Seconds 2
+}
+
+# if there is a link update the shortcut icon
+if($link -ne $null){
+   $x = Get-Hass -object $object
+   if($x.state -match 'on'){ $icon = $onicon }
+   else					{ $icon = $officon   }
+   Set-ShortcutIcon -target $link  -icon $icon
+}
